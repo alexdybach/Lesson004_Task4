153718686d6e2fd62ba41737a71732ae6de0c9ad
Олександр Дибач <alex.dybach.2014@i.ua>
2020-11-05 16:08:14 +0200

    Done lesson004 task4

---
+++ Lesson004_Task4/Lesson004_Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Text.Unicode;
+
+namespace Lesson004_Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Вгадайте число (0 - 10) :");
+            int numb = int.Parse(Console.ReadLine());
+            int r_numb = 5;
+            string cor = "Вгадав";
+            string incor = "Не вгадав";
+            string res;
+
+            res = numb == r_numb ? cor : incor;
+
+            //if (numb == r_numb)
+            //    res = cor;
+            //else
+            //    res = incor;
+
+            Console.WriteLine(res);
+
+        }
+    }
+}
